167a0ad47212e44ff6d47eb7fda23e572c13ea88
saidfaisal <said.faisal@tokopedia.com>
2020-02-19 19:37:50 +0700

    clean code

--- app/src/main/java/com/example/recyclerviewvisitorpattern/BaseViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewvisitorpattern/BaseViewHolder.kt
@@ -4,20 +4,5 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseViewHolder<T>(view: View): RecyclerView.ViewHolder(view) {
-    private var views: HashMap<Int?, View?>? = null
-    private var mItemView: View? = null
-
-    init {
-        mItemView = itemView
-        views = HashMap()
-    }
-
-    fun getView(resId: Int): View? {
-        views.let {
-            val view = it?.get(resId)
-            views?.put(resId, view)
-            return view
-        }
-    }
     abstract fun bind(model: T)
 }
